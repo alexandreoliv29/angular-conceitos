7b2f992af604cea9b850737bff84ac5420c9be5b
ale <alexandre.foj@gmail.com>
2025-11-04 15:54:15 -0300

    ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HelloWorldComponent } from './helloworld/helloworld.component'
+import { Minhapagina } from './minhapagina/minhapagina';
+import { Calculadora } from './calculadora/calculadora';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, Minhapagina, Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
